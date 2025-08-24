24b8373423f1350c05fd153162fd1998fa9dde25
awaramv <129588876+awaramv@users.noreply.github.com>
2025-08-25 00:25:21 +0200

    loggers updated to use a common LOG_FILE path

--- file_manager.py
+++ file_manager.py
@@ -1,15 +1,18 @@
 import shutil
+import datetime
 
+LOG_FILE = "./logs/scheduler.txt"
 
 def sync_BKW_data_with_drive():
     src_file = "/home/awara/dockersinit/logs/daily_detailed_power_output.csv"
     dest_file = "/home/awara/mnt/GoogleDrive/PiDumps/daily_detailed_power_output.csv"
-
     try:
         shutil.copy(src_file, dest_file)
         print(f"File synced successfully from {src_file} to {dest_file}")
+        log_it("BKW", "Success")
     except Exception as e:
         print(f"An error occurred while syncing files: {e}")
+        log_it("DriveSync", "Failure")
 
     src_file = "/home/awara/dockersinit/logs/aggregated_power_output.csv"
     dest_file = "/home/awara/mnt/GoogleDrive/PiDumps/aggregated_power_output.csv"
@@ -17,8 +20,32 @@ def sync_BKW_data_with_drive():
     try:
         shutil.copy(src_file, dest_file)
         print(f"File synced successfully from {src_file} to {dest_file}")
+        log_it("BKW", "Success")
     except Exception as e:
         print(f"An error occurred while syncing files: {e}")
+        log_it("DriveSync", "Failure")
+
+
+def log_it(mode, outcome):
+    now = datetime.datetime.now()
+    if mode == "Pulse":
+        if outcome == "Success":
+            with open(LOG_FILE, "a+") as f:
+                f.write("Scheduler checked for the Jobs last at : \n")
+    elif mode == "BKW":
+        if outcome == "Success":
+            with open(LOG_FILE, "a+") as f:
+                f.write(f"Data read from BKW at {now}\n")
+        elif outcome == "Failure":
+            with open(LOG_FILE, "a+") as f:
+                f.write(f"Failed to read Data read from BKW at {now}\n")
+    elif mode == "DriveSync":
+        if outcome == "Success":
+            with open(LOG_FILE, "a+") as f:
+                f.write(f"File synced successfully at {now}\n")
+        elif outcome == "Failure":
+            with open(LOG_FILE, "a+") as f:
+                f.write(f"Failed to sync files at {now}\n")
 
 
 def sync_log_data_with_drive():
--- tasker.py
+++ tasker.py
@@ -4,7 +4,7 @@ import datetime
 import requests
 import file_manager
 
-LOG_FILE = "./logs/scheduler.txt"
+
 
 
 def read_the_BKW():
@@ -12,16 +12,12 @@ def read_the_BKW():
     try:
         response = requests.get("http://growattreader:5000/update_data")
         if response.status_code == 200:
-            data = response.json()
-            print(f"Data read successfully: {data}")
+            file_manager.log_it("BKW", "Success")
             file_manager.sync_BKW_data_with_drive()
-            print(f"Data synced with Google Drive at {datetime.datetime.now()}")
-            with open(LOG_FILE, "a+") as f:
-                f.write(f"Data read from BKW and Saved to Google Drive at : {now}\n")
+
         else:
             print(f"Failed to read data, status code: {response.status_code}")
-            with open(LOG_FILE, "a+") as f:
-                f.write(f"Failed to read Data read from BKW  : {now}\n")
+            file_manager.log_it("BKW", "Failure")
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while reading data: {e}")
 
@@ -29,14 +25,7 @@ def read_the_BKW():
 schedule.every().day.at("22:11").do(read_the_BKW)
 
 
-def write_current_datetime_to_file():
-    now = datetime.datetime.now()
-    with open(LOG_FILE, "a+") as f:
-        f.write(f"Scheduler checked for the Jobs last at : {now}\n")
-    print(f"Date and time written at {now} to {LOG_FILE}")
-
-
 while True:
-    write_current_datetime_to_file()
+    file_manager.log_it("Pulse", "Success")
     schedule.run_pending()
     time.sleep(60)
