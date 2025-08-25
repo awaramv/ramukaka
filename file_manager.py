9be0b705673b0a08d461c5d6a3086bb2dfa6dc6d
awaramv <129588876+awaramv@users.noreply.github.com>
2025-08-25 19:10:36 +0200

    copy utility modified

--- file_manager.py
+++ file_manager.py
@@ -3,24 +3,25 @@ import datetime
 
 LOG_FILE = "./logs/scheduler.txt"
 
+
 def sync_BKW_data_with_drive():
     src_file = "/home/awara/dockersinit/logs/daily_detailed_power_output.csv"
-    dest_file = "/home/awara/mnt/GoogleDrive/PiDumps/daily_detailed_power_output.csv"
+    dest = "/home/awara/mnt/GoogleDrive/PiDumps"
     try:
-        shutil.copy(src_file, dest_file)
-        print(f"File synced successfully from {src_file} to {dest_file}")
-        log_it("BKW", "Success")
+        shutil.copy(src_file, dest)
+        print(f"File synced successfully from {src_file} to {dest}")
+        log_it("BKW-daily", "Success")
     except Exception as e:
         print(f"An error occurred while syncing files: {e}")
-        log_it("DriveSync", "Failure")
+        log_it("BKW-daily", "Failure")
 
     src_file = "/home/awara/dockersinit/logs/aggregated_power_output.csv"
-    dest_file = "/home/awara/mnt/GoogleDrive/PiDumps/aggregated_power_output.csv"
+    dest = "/home/awara/mnt/GoogleDrive/PiDumps"
 
     try:
-        shutil.copy(src_file, dest_file)
-        print(f"File synced successfully from {src_file} to {dest_file}")
-        log_it("BKW", "Success")
+        shutil.copy(src_file, dest)
+        print(f"File synced successfully from {src_file} to {dest}")
+        log_it("BKW-agg", "Success")
     except Exception as e:
         print(f"An error occurred while syncing files: {e}")
         log_it("DriveSync", "Failure")
@@ -31,8 +32,8 @@ def log_it(mode, outcome):
     if mode == "Pulse":
         if outcome == "Success":
             with open(LOG_FILE, "a+") as f:
-                f.write("Scheduler checked for the Jobs last at : {now} \n")
-    elif mode == "BKW":
+                f.write(f"Scheduler checked for the Jobs last at {now} \n")
+    elif mode == "BKW-agg":
         if outcome == "Success":
             with open(LOG_FILE, "a+") as f:
                 f.write(f"Data read from BKW at {now}\n")
@@ -51,10 +52,10 @@ def log_it(mode, outcome):
 def sync_log_data_with_drive():
     now = datetime.datetime.now()
     src_file = "/home/awara/dockersinit/logs/scheduler.txt"
-    dest_file = "/home/awara/mnt/GoogleDrive/PiDumps/scheduler.txt"
+    dest = "/home/awara/mnt/GoogleDrive/PiDumps"
 
     try:
-        shutil.copy(src_file, dest_file)
+        shutil.copy(src_file, dest)
         print(f"Log file synced successfully at {now}")
         log_it("DriveSync", "Success")
     except Exception as e:
--- tasker.py
+++ tasker.py
@@ -1,15 +1,14 @@
 import schedule
 import time
-import datetime
 import requests
 import file_manager
 
-def read_the_BKW():
-    now = datetime.datetime.now()
+
+def read_the_BKW_daily():
     try:
-        response = requests.get("http://growattreader:5000/update_data")
+        response = requests.get("http://growattreader:5000/update_daily_data")
         if response.status_code == 200:
-            file_manager.log_it("BKW", "Success")
+            file_manager.log_it("BKW-daily", "Success")
             file_manager.sync_BKW_data_with_drive()
 
         else:
@@ -19,8 +18,22 @@ def read_the_BKW():
         print(f"An error occurred while reading data: {e}")
 
 
-schedule.every(10).minutes.do(read_the_BKW)
+def read_the_BKW_aggregated():
+    try:
+        response = requests.get("http://growattreader:5000/update_aggregated_data")
+        if response.status_code == 200:
+            file_manager.log_it("BKW-agg", "Success")
+            file_manager.sync_BKW_data_with_drive()
 
+        else:
+            print(f"Failed to read data, status code: {response.status_code}")
+            file_manager.log_it("BKW", "Failure")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while reading data: {e}")
+
+
+schedule.every(10).minutes.do(read_the_BKW_daily)
+schedule.every(10).minutes.do(read_the_BKW_aggregated)
 schedule.every(1).hours.do(file_manager.sync_log_data_with_drive)
 
 while True:
