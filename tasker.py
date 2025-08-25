d728ee06dc59b18b428c79dc70051bb6423b5bd7
awaramv <129588876+awaramv@users.noreply.github.com>
2025-08-25 18:12:01 +0200

    scheduler check

--- tasker.py
+++ tasker.py
@@ -4,9 +4,6 @@ import datetime
 import requests
 import file_manager
 
-
-
-
 def read_the_BKW():
     now = datetime.datetime.now()
     try:
@@ -22,10 +19,9 @@ def read_the_BKW():
         print(f"An error occurred while reading data: {e}")
 
 
-schedule.every().day.at("22:11").do(read_the_BKW)
-
+schedule.every(10).minutes.do(read_the_BKW)
 
 while True:
     file_manager.log_it("Pulse", "Success")
     schedule.run_pending()
-    time.sleep(60)
+    time.sleep(300)
