cf0f18f03955edc46c683f5821024f0d942680a9
awaramv <129588876+awaramv@users.noreply.github.com>
2025-08-26 21:56:39 +0200

    name changed for file_manager to cleaner

--- butler.py
+++ butler.py
@@ -1,39 +1,39 @@
 import schedule
 import time
 import requests
-import file_manager
+import cleaner
 
 
 def read_the_BKW_daily():
     try:
         response = requests.get("http://growattreader:5000/update_daily_data")
         if response.status_code == 200:
-            file_manager.log_it("BKW-daily", "Success")
-            file_manager.sync_BKW_data_with_drive("BKW-daily")
+            cleaner.log_it("BKW-daily", "Success")
+            cleaner.sync_BKW_data_with_drive("BKW-daily")
 
         else:
-            file_manager.log_it("BKW-daily", "Failure")
+            cleaner.log_it("BKW-daily", "Failure")
     except requests.exceptions.RequestException as e:
-        file_manager.log_it("BKW-daily", "Exception", Exception=e)
+        cleaner.log_it("BKW-daily", "Exception", Exception=e)
 
 
 def read_the_BKW_aggregated():
     try:
         response = requests.get("http://growattreader:5000/update_aggregated_data")
         if response.status_code == 200:
-            file_manager.log_it("BKW-agg", "Success")
-            file_manager.sync_BKW_data_with_drive("BKW-agg")
+            cleaner.log_it("BKW-agg", "Success")
+            cleaner.sync_BKW_data_with_drive("BKW-agg")
 
         else:
             print(f"Failed to read data, status code: {response.status_code}")
-            file_manager.log_it("BKW-agg", "Failure")
+            cleaner.log_it("BKW-agg", "Failure")
     except requests.exceptions.RequestException as e:
-        file_manager.log_it("BKW-daily", "Exception", Exception=e)
+        cleaner.log_it("BKW-daily", "Exception", Exception=e)
 
 
 schedule.every(2).minutes.do(read_the_BKW_daily)
 schedule.every(2).minutes.do(read_the_BKW_aggregated)
-schedule.every(1).hours.do(file_manager.sync_log_data_with_drive)
+schedule.every(1).hours.do(cleaner.sync_log_data_with_drive)
 
 while True:
     schedule.run_pending()
